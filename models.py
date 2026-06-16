06214a7f9083991f0eda0b3ad115e96136abcb7a
Sergey <apoksik4eb@gmail.com>
2026-06-16 13:10:43 +0300

    hw: дополнил реализацию проекта

--- models.py
+++ models.py
@@ -19,7 +19,6 @@ class Category(Base):
     __tablename__ = "categories"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
     name: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
 
     products: Mapped[list["Product"]] = relationship(back_populates="category")
@@ -28,15 +27,10 @@ class Supplier(Base):
     __tablename__ = "suppliers"
    
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
     name: Mapped[str] = mapped_column(Text, nullable=False, unique=True)
-
-    phone: Mapped[str] = mapped_column(Text, nullable=True)
-
-    email: Mapped[str] = mapped_column(Text, unique=True)
-
+    phone: Mapped[Optional[str]] = mapped_column(Text, nullable=True)
+    email: Mapped[Optional[str]] = mapped_column(Text, unique=True, nullable=True)
     is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("TRUE"))
-
     created_at: Mapped[date] = mapped_column(Date, server_default=text("CURRENT_DATE"))
 
     products: Mapped[list["Product"]] = relationship(back_populates="supplier")
@@ -50,46 +44,31 @@ class Product(Base):
     )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
     name: Mapped[str] = mapped_column(Text, nullable=False)
-
     sku: Mapped[str] = mapped_column(Text, unique=True)
-
     category_id: Mapped[Optional[int]] = mapped_column(ForeignKey("categories.id"), nullable=True)
-
     supplier_id: Mapped[Optional[int]] = mapped_column(ForeignKey("suppliers.id"), nullable=True)
-
     purchase_price: Mapped[Optional[Decimal]] = mapped_column(Numeric(12, 2), nullable=True)
-
     selling_price: Mapped[Optional[Decimal]] = mapped_column(Numeric(12, 2), nullable=True)
-
     min_quantity: Mapped[Optional[Decimal]] = mapped_column(Numeric(12, 2), nullable=True)
-
     is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("TRUE"))
-
     created_at: Mapped[date] = mapped_column(Date, server_default=text("CURRENT_DATE"))
 
-    category: Mapped[list["Category"]] = relationship(back_populates="product")
-
-    supplier: Mapped[list["Supplier"]] = relationship(back_populates="product")
-
+    category: Mapped[Optional["Category"]] = relationship(back_populates="products")
+    supplier: Mapped[Optional["Supplier"]] = relationship(back_populates="products")
     stock_movements: Mapped[list["Stock_movement"]] = relationship(back_populates="product")
 
 class Stock_movement(Base):
     __tablename__ = "stock_movements"
     __table_args__ = (
-        CheckConstraint("quantity >= 0", name = "check_quntity_not_negative"),
+        CheckConstraint("quantity > 0", name = "check_quantity_not_negative"),
         CheckConstraint("movement_type in ('IN', 'OUT', 'ADJUST')", name = "check_movement_type_allowed_values")
     )
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-
-    product_id: Mapped[Optional[int]] = mapped_column(ForeignKey("products.id"), nullable=True)
-
+    product_id: Mapped[int] = mapped_column(ForeignKey("products.id"), nullable=False)
     movement_type: Mapped[str] = mapped_column(Text, nullable=False)
-
-    quantity: Mapped[int] = mapped_column(Integer, nullable=True)
-
+    quantity: Mapped[Decimal] = mapped_column(Numeric(12, 3), nullable=False)
     created_at: Mapped[date] = mapped_column(Date, server_default=text("CURRENT_DATE"))
 
-    product: Mapped[list["Product"]] = relationship(back_populates="stock_movement")
+    product: Mapped[list["Product"]] = relationship(back_populates="stock_movements")
